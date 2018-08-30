33fcf840c7ab9a6fcced62191e7abcb64f47bace
satya19051997 <satya.nandy@evossolutions.com>
2018-08-31 02:33:51 +0530

    upload_batch_image.php start

---
+++ upload_batch_image.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="lib/bootstrap.min.js"></script>
+	<title>Upload Batch Image</title>
+</head>
+<body>
+	<div style="text-align: center;">
+		<div style="margin: auto" class="panel panel-primary">
+			<div class="panel-heading">
+				<h1>Upload Batch Image</h1>
+			</div>
+			<div class="panel-body">
+				<p>Uploading Batch Image..</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
